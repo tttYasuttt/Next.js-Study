5fc595f9d43e88f6f246d1e93e0ace2460e879b0
tttYasuttt <bakatesu07071997@gmail.com>
2024-06-18 23:50:59 +0900

    16章完了後、エラー発生中
    16章完了したが、500エラーが発生している。
    ⇒うまく解決できないため、一度すべて再作成しなおす必要あり？？

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,6 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
-import { notFound } from 'next/navigation';
  
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -13,6 +12,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   if (!invoice) {
     notFound();
   }
+
 
   return (
     <main>
